05e16f1307cff837c50fbf43802870947f2f9f1c
Ignacio Lòpez <agsdf12341@gmail.com>
2021-11-25 01:17:53 -0300

    Finished Role Service

--- src/main/java/com/alkemy/ong/repositories/RoleRepository.java
+++ src/main/java/com/alkemy/ong/repositories/RoleRepository.java
@@ -1,8 +1,21 @@
 package com.alkemy.ong.repositories;
 
 import com.alkemy.ong.entities.Role;
+import java.util.List;
+import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long>{
     
+    @Modifying
+    @Query(value = "INSERT INTO roles (id) VALUES (:id)", nativeQuery = true)
+    @Transactional
+    void putId(@Param("id") Long id);
+    
+    List<Role> findByName(String name);
 }
