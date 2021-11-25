650d63e5720bf6d5ddb180e9c0094cb1d1f0afec
IJ-Lopez <82790813+IJ-Lopez@users.noreply.github.com>
2021-11-25 19:11:39 -0300

    Users y roles (#5)
    * User Entity + Role Entity

---
+++ src/main/java/com/alkemy/ong/exceptions/RoleServiceException.java
@@ -0,0 +1,12 @@
+package com.alkemy.ong.exceptions;
+
+public class RoleServiceException extends RuntimeException {
+
+    public RoleServiceException() {
+        
+    }
+
+    public RoleServiceException(String msg) {
+        super(msg);
+    }
+}
