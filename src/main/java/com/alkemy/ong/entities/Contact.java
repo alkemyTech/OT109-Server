7bd34d0f099f40352d124cef82633b98717a692b
Augusto <78941754+aguriona@users.noreply.github.com>
2021-11-25 15:00:07 -0300

    Contacts (#3)
    Entity, repo and service of Contact

---
+++ src/main/java/com/alkemy/ong/entities/Contact.java
@@ -0,0 +1,52 @@
+package com.alkemy.ong.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLUpdate;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "contacts")
+@SQLDelete(sql = "UPDATE posts SET deleted_at = now() WHERE id=?")
+@Where(clause = "deleted_at = null")
+@SQLUpdate(sql = "UPDATE posts SET update_at = now() WHERE id=?")
+public class Contact implements Serializable {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    private String phone;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "message", nullable = false)
+    private String message;
+
+    @Column(name="created_at")
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @Column(name = "deleted_at")
+    private Date deletedAt;
+
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+}
