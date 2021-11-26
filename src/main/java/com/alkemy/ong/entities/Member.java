40c1fda595157798eb912f16125c5cd40bdd1273
PabPol <pabpolval@gmail.com>
2021-11-26 20:48:25 -0300

    Correction of member-organization relation

--- src/main/java/com/alkemy/ong/entities/Member.java
+++ src/main/java/com/alkemy/ong/entities/Member.java
@@ -5,18 +5,17 @@ import java.util.Date;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.SQLDelete;
@@ -59,7 +58,7 @@ public class Member {
     @NotNull
     private String description;
 
-    @OneToOne(cascade = CascadeType.ALL)
+    @ManyToOne
     @JoinColumn(name = "organization_id", referencedColumnName = "organization_id")
     private OrganizationEntity organization;
 
--- src/main/java/com/alkemy/ong/entities/OrganizationEntity.java
+++ src/main/java/com/alkemy/ong/entities/OrganizationEntity.java
@@ -9,15 +9,18 @@ import org.hibernate.annotations.Where;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Email;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "organizations")
@@ -53,8 +56,8 @@ public class OrganizationEntity {
     @Column(columnDefinition = "TEXT", name = "about_us_text")
     private String aboutUsText;
 
-    @OneToOne(mappedBy = "organizarion")
-    private Member member;
+    @OneToMany(mappedBy = "organization" , fetch = FetchType.EAGER)
+    private List<Member> members;
 
     @Temporal(value = TemporalType.TIMESTAMP)
     @CreationTimestamp
