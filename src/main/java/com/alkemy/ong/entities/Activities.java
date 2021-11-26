1171d044d83fbe1d0536932c3ccf66686bacae5f
dellmdq <erikdell@gmail.com>
2021-11-26 13:26:15 -0300

    Initial commit

---
+++ src/main/java/com/alkemy/ong/entities/Activities.java
@@ -0,0 +1,17 @@
+package com.alkemy.ong.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "activity")
+public class Activities {
+
+}
