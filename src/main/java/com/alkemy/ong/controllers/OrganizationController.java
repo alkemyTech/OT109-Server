38777b5cc0a2b718e71844c750f87a2f41f8cf03
Ignacio Lòpez <agsdf12341@gmail.com>
2021-12-01 04:09:08 -0300

    Finished Organization Get Controller Method
    Cree un DTO para enviar a través del método. Además, implementé "serializable" en la entidad de organización.

---
+++ src/main/java/com/alkemy/ong/controllers/OrganizationController.java
@@ -0,0 +1,44 @@
+package com.alkemy.ong.controllers;
+
+import com.alkemy.ong.entities.OrganizationEntity;
+import com.alkemy.ong.pojos.output.ListOrganizationDTO;
+import com.alkemy.ong.services.OrganizationService;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/organization")
+public class OrganizationController {
+    
+    @Autowired
+    private OrganizationService orgService;
+    
+    @GetMapping("/public")
+    @ResponseStatus(HttpStatus.OK)
+    public List<ListOrganizationDTO> findAll(){
+        List<ListOrganizationDTO> response = new ArrayList();
+        List<OrganizationEntity> organizations = orgService.findAll();
+        organizations.forEach(e -> {
+           ListOrganizationDTO organization = new ListOrganizationDTO();
+           BeanUtils.copyProperties(e, organization);
+           response.add(organization);
+        });
+        return response;
+    }
+    
+    @PostMapping("/public")
+    @ResponseStatus(HttpStatus.OK)
+    public void update(){
+        //FALTA LA VALIDACIÓN DE ADMINISTRADOR
+        
+    }
+    
+}
--- src/main/java/com/alkemy/ong/entities/OrganizationEntity.java
+++ src/main/java/com/alkemy/ong/entities/OrganizationEntity.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.entities;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Column;
@@ -18,7 +19,6 @@ import javax.validation.constraints.Email;
 import java.util.Date;
 import java.util.List;
 
-
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -27,56 +27,53 @@ import org.hibernate.annotations.Where;
 import lombok.Getter;
 import lombok.Setter;
 
-
 @Entity
 @Table(name = "organizations")
 @Getter
 @Setter
 @SQLDelete(sql = "UPDATE organizations SET deleted_at = current_timestamp() WHERE id = ?")
 @Where(clause = "deleted_at = null")
+public class OrganizationEntity implements Serializable {
 
-public class OrganizationEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "organization_id")
+    private Long id;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "organization_id")
-	private Long id;
+    @Column(nullable = false)
+    private String name;
 
-	@Column(nullable = false)
-	private String name;
+    @Column(nullable = false)
+    private String image;
 
-	@Column(nullable = false)
-	private String image;
+    private String address;
 
-	private String address;
+    private Integer phone;
 
-	private Integer phone;
+    @Column(nullable = false)
+    @Email
+    private String email;
 
-	@Column(nullable = false)
-	@Email
-	private String email;
+    @Column(columnDefinition = "TEXT", name = "welcome_text", nullable = false)
+    private String welcomeText;
 
-	@Column(columnDefinition = "TEXT", name = "welcome_text", nullable = false)
-	private String welcomeText;
+    @Column(columnDefinition = "TEXT", name = "about_us_text")
+    private String aboutUsText;
 
-	@Column(columnDefinition = "TEXT", name = "about_us_text")
-	private String aboutUsText;
-
-
-    @OneToMany(mappedBy = "organization" , fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "organization", fetch = FetchType.EAGER)
     private List<Member> members;
 
-	@Temporal(value = TemporalType.TIMESTAMP)
-	@CreationTimestamp
-	@Column(name = "created_at")
-	private Date createdAt;
+    @Temporal(value = TemporalType.TIMESTAMP)
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private Date createdAt;
 
-	@Temporal(value = TemporalType.TIMESTAMP)
-	@UpdateTimestamp
-	@Column(name = "updated_at")
-	private Date updatedAt;
+    @Temporal(value = TemporalType.TIMESTAMP)
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
 
-	@Temporal(value = TemporalType.TIMESTAMP)
-	@Column(name = "deleted_at")
-	private Date deletedAt;
+    @Temporal(value = TemporalType.TIMESTAMP)
+    @Column(name = "deleted_at")
+    private Date deletedAt;
 }
