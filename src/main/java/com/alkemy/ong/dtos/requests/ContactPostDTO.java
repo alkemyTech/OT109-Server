66620cdbef7af2542ee1645fc01e688d2044f4d0
dellmdq <erikdell@gmail.com>
2021-12-04 01:59:02 -0300

    Added annotations ContactPostDTO

--- src/main/java/com/alkemy/ong/dtos/requests/ContactPostDTO.java
+++ src/main/java/com/alkemy/ong/dtos/requests/ContactPostDTO.java
@@ -3,14 +3,24 @@ package com.alkemy.ong.dtos.requests;
 import com.alkemy.ong.entities.Contact;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import javax.validation.constraints.NotEmpty;
 
 @Builder
 @Data
 public class ContactPostDTO {
 
+    @NotEmpty
     private String name;
+
     private String phone;
+
+    @NotEmpty
     private String email;
+
+    @NotEmpty
     private String message;
 
     public Contact toContact(){
