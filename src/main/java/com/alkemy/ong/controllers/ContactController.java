08dc9dc81cb6724e25f181c0697c8682db290125
Erik Dell <erikdell@gmail.com>
2021-12-05 21:28:41 -0300

    contacts GET and POST endpoints (#23)
    * Saving just in case
    
    * FILE_NAME npe
    
    * FILE_NAME npe
    
    * S3 Post and Get Request Working
    
    * probando git
    
    * checking github config
    
    * Git OK
    
    * Just saving
    
    * Save Fix. Now returns file path as a String
    
    * Security Fix
    
    * application.properties recovered
    
    * pom.xml recovered
    
    * S3Controller Swagger fix
    
    * Deleted Comments
    
    * utils package created.
      ValidatorUtil class created.
        isEmailValid() created.
    
    ContactController created.
    ContactPostDto created.
    
    * added Phone Number Validation isPhoneValid() at ValidatorUtil
    
    * Fix Contact @Where annotation
    Created ContactListDTO
    
    contacts GET and POST endpoints finished
    
    * Created toContact() in ContactPostDTO to avoid boilerplate code.
    
    * Added annotations ContactPostDTO
    
    * ContactController exceptions added and NullPointerException catched.

---
+++ src/main/java/com/alkemy/ong/controllers/ContactController.java
@@ -0,0 +1,63 @@
+package com.alkemy.ong.controllers;
+
+import com.alkemy.ong.dtos.requests.ContactPostDTO;
+import com.alkemy.ong.dtos.responses.ContactListDTO;
+import com.alkemy.ong.entities.Contact;
+import com.alkemy.ong.services.ContactService;
+import com.alkemy.ong.utils.ValidatorUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.Null;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/contacts")
+public class ContactController {
+
+    @Autowired
+    private ContactService contactService;
+    @Autowired
+    private ValidatorUtil validatorUtil;
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody ContactPostDTO contactPostDto){
+        Contact contactCreated;
+        try {
+            if (contactPostDto.getName().isEmpty()) {
+                return new ResponseEntity<>("Name cannot be empty.", HttpStatus.BAD_REQUEST);
+            }
+            if (!validatorUtil.isEmailValid(contactPostDto.getEmail())) {
+                return new ResponseEntity<>("Invalid email address.", HttpStatus.BAD_REQUEST);
+            }
+            if (!validatorUtil.isPhoneValid(contactPostDto.getPhone())) {
+                return new ResponseEntity<>("Invalid phone number.", HttpStatus.BAD_REQUEST);
+            }
+            if(contactPostDto.getMessage().isEmpty()){
+                return new ResponseEntity<>("Invalid message.", HttpStatus.BAD_REQUEST);
+            }
+            Contact contactToCreate = contactPostDto.toContact();
+            contactCreated = contactService.createContact(contactToCreate);
+        }catch(NullPointerException npe){
+            System.out.println("Name, email, phone number and message cannot be empty.");
+            return new ResponseEntity<>("Name, email, phone number and message cannot be empty.",HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(contactCreated, HttpStatus.CREATED);
+    }
+
+    /**Falta validación como administrador*/
+    @GetMapping
+    public List<ContactListDTO> getAll(){
+        //Falta validación como administrador
+        List<Contact> contactList = contactService.findAllContacts();
+        List<ContactListDTO> contactDTOList = new ArrayList<>();
+        for (Contact c : contactList) {
+            contactDTOList.add(new ContactListDTO(c));
+        }
+        return contactDTOList;
+    }
+}
