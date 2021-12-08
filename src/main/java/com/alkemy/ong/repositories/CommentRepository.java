a914a7dfd6d7bd757ad1e3e1b33fb26be16ae373
dellmdq <erikdell@gmail.com>
2021-12-08 17:48:46 -0300

    TypedQuery implemented to get table columns needed to create CommentListDTO.

--- src/main/java/com/alkemy/ong/repositories/CommentRepository.java
+++ src/main/java/com/alkemy/ong/repositories/CommentRepository.java
@@ -1,10 +1,15 @@
 package com.alkemy.ong.repositories;
 
+import com.alkemy.ong.dtos.responses.CommentListDTO;
 import com.alkemy.ong.entities.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.TypedQuery;
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+    List<CommentListDTO> findCommentsByNewsId(Long id);
 }
