2c4305ac68f691baccb13e6b1217f2d5cee1e914
dellmdq <erikdell@gmail.com>
2021-12-08 22:08:24 -0300

    Added email to CommentListDTO. Changes to implement this attribute on DTO and Query.

--- src/main/java/com/alkemy/ong/dtos/responses/CommentListDTO.java
+++ src/main/java/com/alkemy/ong/dtos/responses/CommentListDTO.java
@@ -6,9 +6,11 @@ import lombok.Data;
 public class CommentListDTO {
     private Long id;
     private String body;
+    private String email;
 
-    public CommentListDTO(Long id, String body) {
+    public CommentListDTO(Long id, String body, String email) {
         this.id = id;
         this.body = body;
+        this.email = email;
     }
 }
