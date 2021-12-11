f5cbd56662c6d87429c61c07f4482b9502cc0296
Yane05 <69738680+Yane05@users.noreply.github.com>
2021-12-11 10:28:14 -0300

    comment post (#34)
    * comment post
    
    * arreglo de commentService

---
+++ src/main/java/com/alkemy/ong/dtos/responses/CommentDTO.java
@@ -0,0 +1,13 @@
+package com.alkemy.ong.dtos.responses;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CommentDTO {
+    private Long id;
+    private Long new_id;
+    private Long user_id;
+    private String body;
+}
