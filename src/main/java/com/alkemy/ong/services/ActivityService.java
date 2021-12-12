d272feded899cfb12836cd1f31548adf85988efb
Pablo Polanco Valdes <51938148+Pabpol@users.noreply.github.com>
2021-12-12 11:14:39 -0300

    Data seeder for activity (#43)
    * Data seeder for activity
    
    * Seeder for ong activity
    
    * Validation for activity data seeder

--- src/main/java/com/alkemy/ong/services/ActivityService.java
+++ src/main/java/com/alkemy/ong/services/ActivityService.java
@@ -16,33 +16,38 @@ public class ActivityService {
     @Autowired
     private ActivityRepository activityRepository;
 
-    public Activity create(Activity activity){
+    public Activity create(Activity activity) {
         activity.setCreatedAt(new Date());
         return activityRepository.save(activity);
     }
 
-    public Activity update(Activity activity){
+    public Activity update(Activity activity) {
         activity.setUpdatedAt(new Date());
         return activityRepository.save(activity);
     }
 
-    public Activity findById(Long id){
-        return activityRepository.findById(id).orElseThrow( () -> (new ActivityServiceException("Activity with id: " + id + " not found.")));
+    public Activity findById(Long id) {
+        return activityRepository.findById(id)
+                .orElseThrow(() -> (new ActivityServiceException("Activity with id: " + id + " not found.")));
     }
 
-    public Activity findByName(String name){
-        return activityRepository.findByName(name).orElseThrow( () -> (new ActivityServiceException("Activity with name: " + name + " not found.")));
+    public Activity findByName(String name) {
+        return activityRepository.findByName(name)
+                .orElseThrow(() -> (new ActivityServiceException("Activity with name: " + name + " not found.")));
     }
 
-    public Activity delete(Long id){
+    public Activity delete(Long id) {
         Activity activityToSoftDelete = this.findById(id);
         activityToSoftDelete.setDeletedAt(new Date());
         return activityRepository.save(activityToSoftDelete);
     }
 
-    public List<Activity> findAll(){
+    public List<Activity> findAll() {
         return activityRepository.findByDeletedAtIsNull();
     }
 
+    public boolean existsByName(String name) {
+        return activityRepository.existsByName(name);
+    }
 
 }
