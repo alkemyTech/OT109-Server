dd4c0a25af5cdb04b443dc2fe63dd2098cf1f02c
dellmdq <erikdell@gmail.com>
2021-12-12 13:10:45 -0300

    Just saving.

--- src/main/java/com/alkemy/ong/controllers/CommentController.java
+++ src/main/java/com/alkemy/ong/controllers/CommentController.java
@@ -5,27 +5,23 @@ import com.alkemy.ong.dtos.requests.CommentPostRequestDTO;
 import com.alkemy.ong.dtos.responses.CategoryDTO;
 import com.alkemy.ong.dtos.responses.CommentDTO;
 import com.alkemy.ong.dtos.responses.CommentListDTO;
+import com.alkemy.ong.entities.Comment;
 import com.alkemy.ong.services.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.Min;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("comments")
+@RequestMapping("/comments")
 public class CommentController {
 
     @Autowired
@@ -39,11 +35,20 @@ public class CommentController {
         }
         return new ResponseEntity<>(commentList, HttpStatus.OK);
     }
-        @PostMapping
-        public ResponseEntity<CommentDTO> create(@Valid @RequestBody CommentPostRequestDTO commentPostRequestDTO){
-            CommentDTO commentCreated = commentService.create(commentPostRequestDTO);
-            return ResponseEntity.status(HttpStatus.CREATED).body(commentCreated);
-        }
+
+    @PostMapping
+    public ResponseEntity<CommentDTO> create(@Valid @RequestBody CommentPostRequestDTO commentPostRequestDTO){
+        CommentDTO commentCreated = commentService.create(commentPostRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(commentCreated);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Comment> delete(@PathVariable @Min(value = 1, message = "Id must be equal or greater than 1") Long id){
+
+        return null;
+    }
+
+
 
 
 }
