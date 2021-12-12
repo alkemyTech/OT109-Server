0ef5e05cb77f193753e7dea8756687082e54993f
dellmdq <erikdell@gmail.com>
2021-12-12 14:26:04 -0300

    Merge conflicts resolved.

--- src/main/java/com/alkemy/ong/controllers/CommentController.java
+++ src/main/java/com/alkemy/ong/controllers/CommentController.java
@@ -1,27 +1,20 @@
 package com.alkemy.ong.controllers;
 
-import com.alkemy.ong.dtos.requests.CategoryPostPutRequestDTO;
 import com.alkemy.ong.dtos.requests.CommentPostRequestDTO;
-import com.alkemy.ong.dtos.responses.CategoryDTO;
 import com.alkemy.ong.dtos.responses.CommentDTO;
 import com.alkemy.ong.dtos.responses.CommentListDTO;
-import com.alkemy.ong.entities.Comment;
 import com.alkemy.ong.services.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
-
 @RestController
-@RequestMapping("/comments")
+@RequestMapping
 public class CommentController {
 
     @Autowired
@@ -36,7 +29,7 @@ public class CommentController {
         return new ResponseEntity<>(commentList, HttpStatus.OK);
     }
 
-    @PostMapping
+    @PostMapping("/comments")
     public ResponseEntity<CommentDTO> create(@Valid @RequestBody CommentPostRequestDTO commentPostRequestDTO) {
         CommentDTO commentCreated = commentService.create(commentPostRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(commentCreated);
--- src/main/java/com/alkemy/ong/dtos/requests/CommentPostRequestDTO.java
+++ src/main/java/com/alkemy/ong/dtos/requests/CommentPostRequestDTO.java
@@ -2,13 +2,10 @@ package com.alkemy.ong.dtos.requests;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @Data
 @Builder
