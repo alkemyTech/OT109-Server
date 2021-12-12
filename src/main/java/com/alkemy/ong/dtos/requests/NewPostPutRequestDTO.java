cb73cf7ca061979c7b3576970f5b0e077e418861
Mirella Ponce <mirealponce1407@gmail.com>
2021-12-11 22:55:06 -0300

    POST Y PUT Activities actualización

--- src/main/java/com/alkemy/ong/dtos/requests/NewPostPutRequestDTO.java
+++ src/main/java/com/alkemy/ong/dtos/requests/NewPostPutRequestDTO.java
@@ -3,14 +3,20 @@ package com.alkemy.ong.dtos.requests;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class NewPostPutRequestDTO {
+    @NotNull(message = "Name may not be empty")
     private String name;
 
+    @NotNull(message = "Content may not be empty")
     private String content;
 
+    @NotNull(message = "Image may not be empty")
     private String image;
 
+    @NotNull(message = "Category may not be empty")
     private Long categoryId;
 }
