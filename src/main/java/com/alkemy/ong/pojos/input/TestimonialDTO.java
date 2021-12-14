eebbaf633eeeaa66987f6b2be651750985d2d665
dellmdq <erikdell@gmail.com>
2021-12-14 13:30:31 -0300

    TestimonialDTO validations fix.

--- src/main/java/com/alkemy/ong/pojos/input/TestimonialDTO.java
+++ src/main/java/com/alkemy/ong/pojos/input/TestimonialDTO.java
@@ -2,16 +2,19 @@ package com.alkemy.ong.pojos.input;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+@Builder
 public class TestimonialDTO {
 
+    @NotBlank
+    @Size(min = 3, message = "Name should be at least three characters")
     private String name;
     private String image;
     private String content;
