c1530ff605d9fcef3acf8b272d026dfbe6d96f1c
Ignacio Lòpez <agsdf12341@gmail.com>
2021-12-18 22:17:09 -0300

    Finished Controller Testing
    Modifique además el controlador, servicio y los DTOs necesarios para cumplir con las necesidades del programa

--- src/test/java/com/alkemy/ong/testController/UserControllerTest.java
+++ src/test/java/com/alkemy/ong/testController/UserControllerTest.java
@@ -1,22 +1,17 @@
 package com.alkemy.ong.testController;
 
 import com.alkemy.ong.entities.User;
-import com.alkemy.ong.pojos.input.RequestLoginDTO;
-import com.alkemy.ong.pojos.output.ListUserDTO;
-import com.alkemy.ong.pojos.output.ResponseLoginDTO;
+import com.alkemy.ong.exceptions.UserServiceException;
+import com.alkemy.ong.pojos.input.UpdateUserDTO;
 import com.alkemy.ong.services.UserService;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import java.nio.charset.Charset;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import javax.transaction.Transactional;
-import net.minidev.json.JSONArray;
-import org.json.JSONObject;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -27,7 +22,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -35,63 +29,163 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.hamcrest.Matchers.instanceOf;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.security.test.context.support.WithUserDetails;
-import org.springframework.util.MultiValueMap;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestInstance(Lifecycle.PER_CLASS)
+@ExtendWith(SpringExtension.class)
 public class UserControllerTest {
-    
+
     @Autowired
     private MockMvc mockMvc;
-    
-    @MockBean
+
+    @Autowired
     private UserService userService;
-    
+
+    private ObjectMapper mapper;
+
+    private ObjectWriter writer;
+
     private static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
-    
+
     @BeforeAll
-    public void setup() throws Exception{
-        
+    public void setup() throws Exception {
+        mapper = new ObjectMapper().configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        writer = mapper.writer().withDefaultPrettyPrinter();
     }
-    
+
     @Test
-    public void findAllWithoutAuthorityShouldReturnForbidden() throws Exception{
+    public void findAllWithoutAuthorityShouldReturnForbidden() throws Exception {
         this.mockMvc.perform(get("/users"))
                 .andDo(print())
                 .andExpect(status().isForbidden());
     }
-    
+
     @Test
     @WithUserDetails(value = "juanperez@gmail.com")
-    public void findAllWithRegularAuthorityShouldReturnUserList() throws Exception{
+    public void findAllWithRegularAuthorityShouldReturnUserList() throws Exception {
         this.mockMvc.perform(get("/users"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
                 .andExpect(jsonPath("$", instanceOf(List.class)));
     }
-    
+
     @Test
     @WithUserDetails(value = "monicasala@gmail.com")
-    public void findAllWithAdminAuthorityShouldReturnUserList() throws Exception{
+    public void findAllWithAdminAuthorityShouldReturnUserList() throws Exception {
         this.mockMvc.perform(get("/users"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
                 .andExpect(jsonPath("$", instanceOf(List.class)));
     }
-    
+
     @Test
     @Transactional
     public void updateWithoutAuthorityShouldReturnForbidden() throws Exception {
-        this.mockMvc.perform(patch("/users/"))
+        this.mockMvc.perform(patch("/users/1"))
                 .andDo(print())
                 .andExpect(status().isForbidden());
     }
-    
-    
-    
+
+    @Test
+    @Transactional
+    @WithUserDetails(value = "juanperez@gmail.com")
+    public void updateWithRegularAuthorityShouldReturnForbidden() throws Exception {
+        this.mockMvc.perform(patch("/users/1"))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @Transactional
+    @WithUserDetails(value = "monicasala@gmail.com")
+    public void updateWithAdminAuthorityShouldReturnOk() throws Exception {
+        UpdateUserDTO userUpdate = UpdateUserDTO.builder()
+                .firstName("updatedFirstName")
+                .lastName("updatedLastName")
+                .role("USER")
+                .build();
+
+        this.mockMvc.perform(patch("/users/1")
+                .content(writer.writeValueAsString(userUpdate))
+                .contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        User user = this.userService.findById(1L);
+        assertThat(user.getFirstName().equals(userUpdate.getFirstName()));
+        assertThat(user.getLastName().equals(userUpdate.getLastName()));
+        assertThat(user.getRole().getName().equals(userUpdate.getRole()));
+    }
+
+    @Test
+    @Transactional
+    @WithUserDetails(value = "monicasala@gmail.com")
+    public void updateWithoutValidBodyShouldReturnBadRequest() throws Exception {
+        UpdateUserDTO userUpdate;
+        this.mockMvc.perform(patch("/users/1"))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+
+        userUpdate = UpdateUserDTO.builder().email("email").build();
+        this.mockMvc.perform(patch("/users/1")
+                .content(writer.writeValueAsString(userUpdate))
+                .contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+
+        userUpdate = UpdateUserDTO.builder().photo("www.google.com").build();
+        this.mockMvc.perform(patch("/users/1")
+                .content(writer.writeValueAsString(userUpdate))
+                .contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+
+        userUpdate = UpdateUserDTO.builder().photo(".jpg").build();
+        this.mockMvc.perform(patch("/users/1")
+                .content(writer.writeValueAsString(userUpdate))
+                .contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+
+        userUpdate = UpdateUserDTO.builder().role("ROLE").build();
+        this.mockMvc.perform(patch("/users/1")
+                .content(writer.writeValueAsString(userUpdate))
+                .contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @Transactional
+    public void deleteWithoutAuthorityShouldReturnForbidden() throws Exception {
+        this.mockMvc.perform(delete("/users/1"))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @Transactional
+    @WithUserDetails(value = "juanperez@gmail.com")
+    public void deleteWithRegularAuthorityShouldReturnForbidden() throws Exception {
+        this.mockMvc.perform(delete("/users/1"))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @Transactional
+    @WithUserDetails(value = "monicasala@gmail.com")
+    public void DeleteWithAdminAuthorityShouldReturnOk() throws Exception {
+        this.mockMvc.perform(delete("/users/1"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        assertThat(assertThrows(UserServiceException.class, () -> this.userService.findById(1L)).getMessage().equals("User not found"));
+    }
 }
