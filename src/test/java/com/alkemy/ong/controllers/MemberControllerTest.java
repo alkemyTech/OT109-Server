ed56aae817e58146670728dec0a7ab1addbb15f8
dellmdq <erikdell@gmail.com>
2021-12-20 17:56:54 -0300

    add error test for all endpoint

--- src/test/java/com/alkemy/ong/controllers/MemberControllerTest.java
+++ src/test/java/com/alkemy/ong/controllers/MemberControllerTest.java
@@ -5,6 +5,7 @@ import com.alkemy.ong.dtos.responses.ListMemberDTO;
 import com.alkemy.ong.dtos.responses.MemberResponseDTO;
 import com.alkemy.ong.entities.OrganizationEntity;
 import com.alkemy.ong.exceptions.DataAlreadyExistException;
+import com.alkemy.ong.exceptions.NotFoundException;
 import com.alkemy.ong.pojos.input.RequestLoginDTO;
 import com.alkemy.ong.pojos.output.ListOrganizationDTO;
 import com.alkemy.ong.services.MemberService;
@@ -24,15 +25,15 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
@@ -60,7 +61,7 @@ public class MemberControllerTest {
     }
 
     @Test
-    void authRequestOnLoginWithAdmin_shouldSucceedWith200() throws Exception{
+    void authRequestOnLoginWithAdmin_shouldSucceedWith200() throws Exception {
         //Given
         RequestLoginDTO requestLoginDTO = new RequestLoginDTO();
         requestLoginDTO.setUsername("admin@admin.com");
@@ -69,34 +70,35 @@ public class MemberControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.post("/auth/login")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(requestLoginDTO)))
-        //then
+                //then
                 .andExpect(status().isOk());
     }
 
-   @Test
-   @WithUserDetails(value = "admin@admin.com")
-   void findAll() throws Exception{
+    @Test
+    @WithUserDetails(value = "admin@admin.com")
+    void findAllWithAdminAuthShouldReturnMemberList() throws Exception {
         //given
-       SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-       Date datenow = sdf.parse("2021-12-17 17:38:02");
-       Date datenow1 = sdf.parse("2020-10-19 21:15:50");
-       Date datenow2 = sdf.parse("2021-12-11 09:22:12");
-       OrganizationEntity ong1 = new OrganizationEntity(1L, "ONG1");
-       ListMemberDTO member0 = new ListMemberDTO(1L,"Pepe","facebook.com","instagram.com","linkedin.com","image.jpg","description", ong1 , datenow2, null, null);
-       ListMemberDTO member1 = new ListMemberDTO(2L,"Jose","facebook.com","instagram.com","linkedin.com","image.jpg","description", ong1, datenow1, null, null);
-       ListMemberDTO member2 = new ListMemberDTO(3L,"Marcela","facebook.com","instagram.com","linkedin.com","image.jpg","description", ong1, datenow, null, null);
-       ListMemberDTO member3 = new ListMemberDTO(4L,"Maria","facebook.com","instagram.com","linkedin.com","image.jpg","description", ong1, datenow, null, null);
-       List<ListMemberDTO> memberDTOS = Arrays.asList(member0, member1, member2, member3);
-       System.out.println(memberDTOS);
-       when(memberService.findAll()).thenReturn(memberDTOS);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date datenow = sdf.parse("2021-12-17 17:38:02");
+        Date datenow1 = sdf.parse("2020-10-19 21:15:50");
+        Date datenow2 = sdf.parse("2021-12-11 09:22:12");
+        OrganizationEntity ong1 = new OrganizationEntity(1L, "ONG1");
+        ListMemberDTO member0 = new ListMemberDTO(1L, "Pepe", "facebook.com", "instagram.com", "linkedin.com", "image.jpg", "description", ong1, datenow2, null, null);
+        ListMemberDTO member1 = new ListMemberDTO(2L, "Jose", "facebook.com", "instagram.com", "linkedin.com", "image.jpg", "description", ong1, datenow1, null, null);
+        ListMemberDTO member2 = new ListMemberDTO(3L, "Marcela", "facebook.com", "instagram.com", "linkedin.com", "image.jpg", "description", ong1, datenow, null, null);
+        ListMemberDTO member3 = new ListMemberDTO(4L, "Maria", "facebook.com", "instagram.com", "linkedin.com", "image.jpg", "description", ong1, datenow, null, null);
+        List<ListMemberDTO> memberDTOS = Arrays.asList(member0, member1, member2, member3);
+        System.out.println(memberDTOS);
+        when(memberService.findAll()).thenReturn(memberDTOS);
 
-       //when
-       mockMvc.perform(MockMvcRequestBuilders
-                .get("/members")
-                .contentType(MediaType.APPLICATION_JSON))
-       //Then
+        //when
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/members")
+                        .contentType(MediaType.APPLICATION_JSON))
+                //Then
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(4)))
                 .andExpect(jsonPath("$[0].id").value(1L))
                 .andExpect(jsonPath("$[0].name").value("Pepe"))
                 .andExpect(jsonPath("$[0].organization.id").value(1L))
@@ -104,31 +106,92 @@ public class MemberControllerTest {
                 .andExpect(jsonPath("$[0].deletedAt").value(IsNull.nullValue()))//chequear que todos tengan deletedAtNull;
                 .andExpect(content().json(objectMapper.writeValueAsString(memberDTOS)));
 
-       verify(memberService).findAll();
-   }
+        verify(memberService).findAll();
+    }
 
-   @Test
-   @WithUserDetails(value = "admin@admin.com")
-   void save() throws Exception {
+    @Test
+    @WithUserDetails(value = "juanperez@gmail.com")
+    void findAllWithUserAuthShouldReturnForbidden403Status() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/members"))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithUserDetails(value = "admin@admin.com")
+    void save() throws Exception {
         //given
-       MemberRequest member = new MemberRequest("Teto", "facebook.com", "instagram.com", "linkedin.com", "image.jpg", "description", 1L);
-       ListOrganizationDTO ong1 = new ListOrganizationDTO(1L,"ONG1");
-       MemberResponseDTO memberResponse = new MemberResponseDTO(1L,"Teto", "facebook.com", "instagram.com", "linkedin.com", "image.jpg", "description", ong1);
-       when(memberService.create(any())).thenReturn(memberResponse);
+        MemberRequest member = new MemberRequest("Teto", "facebook.com", "instagram.com", "linkedin.com", "image.jpg", "description", 1L);
+        ListOrganizationDTO ong1 = new ListOrganizationDTO(1L, "ONG1");
+        MemberResponseDTO memberResponse = new MemberResponseDTO(1L, "Teto", "facebook.com", "instagram.com", "linkedin.com", "image.jpg", "description", ong1);
+        when(memberService.create(any())).thenReturn(memberResponse);
         //when
-       mockMvc.perform(MockMvcRequestBuilders
-               .post("/members").contentType(MediaType.APPLICATION_JSON)
-               .content(objectMapper.writeValueAsString(memberResponse)))
-        //then
-               .andExpect(status().isCreated())
-               .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-               .andExpect(jsonPath("$.id").value(1L))
-               .andExpect(jsonPath("$.name").value("Teto"))
-               .andExpect(jsonPath("$.organization.id").value(1L))
-               .andExpect(jsonPath("$.organization.name").value("ONG1"));
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/members").contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(member)))
+                //then
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.name").value("Teto"))
+                .andExpect(jsonPath("$.organization.id").value(1L))
+                .andExpect(jsonPath("$.organization.name").value("ONG1"));
 
-       verify(memberService).create(any());
-   }
+        verify(memberService).create(any());
+    }
+
+
+    @Test
+    @WithUserDetails(value = "juanperez@gmail.com")
+    void saveWithUserAuthShouldReturn403ForbiddenStatus() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/members"))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithUserDetails(value = "admin@admin.com")
+    void saveMemberWithNullOrganizationShouldReturnErrorMessage() throws Exception {
+        MemberRequest member = new MemberRequest("Teto", "facebook.com", "instagram.com", "linkedin.com", "image.jpg", "description", null);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/members")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(member)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors").value("organizationId: must not be null"));
+    }
+
+    @Test
+    @WithUserDetails(value = "admin@admin.com")
+    void saveMemberWithNonExistentOrganizationIdShouldReturnNotFound404Status() throws Exception {
+        //given
+        MemberRequest member = new MemberRequest("Teto", "facebook.com", "instagram.com", "linkedin.com", "image.jpg", "description", 99L);
+        //when
+        when(memberService.create(any())).thenThrow(NotFoundException.class);
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.post("/members")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(member)))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+        verify(memberService).create(any());
+    }
+
+    @Test
+    @WithUserDetails(value = "admin@admin.com")
+    void saveMemberThatAlreadyExistShouldReturn409ConflictStatus() throws Exception{
+        //given
+        MemberRequest member = new MemberRequest("Erik", "facebook.com", "instagram.com", "linkedin.com", "image.jpg", "description", 1L);
+        //when
+        when(memberService.create(any())).thenThrow(DataAlreadyExistException.class);
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.post("/members")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(member)))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isConflict());//https://stackoverflow.com/questions/3825990/http-response-code-for-post-when-resource-already-exists
+
+        verify(memberService).create(any());
+    }
 
     @Test
     @WithUserDetails(value = "admin@admin.com")
@@ -141,29 +204,83 @@ public class MemberControllerTest {
     }
 
     @Test
+    @WithUserDetails(value = "juanperez@gmail.com")
+    void deleteMemberWithUserAuthShouldReturn403Forbidden() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/members/1"))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithUserDetails(value = "admin@admin.com")
+    void deleteNonExistentMemberShouldReturn404NotFound() throws Exception{
+        //when
+        doThrow(NotFoundException.class).when(memberService).delete(any());
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.delete("/members/9999"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
     @WithUserDetails(value = "admin@admin.com")
     void update() throws Exception {
         //given
-        MemberRequest oldMember = new MemberRequest("Tito", "facebook.com", "instagram.com", "linkedin.com", "image.jpg", "description", 1L);
-        mockMvc.perform(MockMvcRequestBuilders
-                .post("/members")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(oldMember)));
+//        MemberRequest oldMember = new MemberRequest("Tito", "facebook.com", "instagram.com", "linkedin.com", "image.jpg", "description", 1L);
+//        mockMvc.perform(MockMvcRequestBuilders
+//                .post("/members")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(objectMapper.writeValueAsString(oldMember)));
 
         MemberRequest newMember = new MemberRequest("Tito", "facebook.com", "instagram.com", "linkedin.com", "image.jpg", "NEW description", 1L);
         ListOrganizationDTO ong1 = new ListOrganizationDTO(1L, "ONG1");
         MemberResponseDTO newMemberResponse = new MemberResponseDTO(1L, "Tito", "facebook.com", "instagram.com", "linkedin.com", "image.jpg", "NEW description", ong1);
-        when(memberService.update(newMember,1L)).thenReturn(newMemberResponse);
+        when(memberService.update(newMember, 1L)).thenReturn(newMemberResponse);
 
         //when
         mockMvc.perform(MockMvcRequestBuilders
-                .put("/members/1")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(newMemberResponse)))
+                        .put("/members/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newMember)))
         //then
                 .andExpect(status().isOk());
 
-        verify(memberService).update(any(),any());
+        verify(memberService).update(any(), any());
+    }
 
+    @Test
+    @WithUserDetails(value = "juanperez@gmail.com")
+    void updateMemberWithUserAuthShouldReturn403Forbidden() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.put("/members/1"))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithUserDetails(value = "admin@admin.com")
+    void updateNonExistentMemberShouldReturn404NotFound() throws Exception{
+        //given
+        MemberRequest newMember = new MemberRequest("Tito", "facebook.com", "instagram.com", "linkedin.com", "image.jpg", "NEW description", 1L);
+        //when
+        doThrow(NotFoundException.class).when(memberService).update(any(),any());
+        //then
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put("/members/9999")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newMember)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @WithUserDetails(value = "admin@admin.com")
+    void updateMemberWithNullOrganizationShouldReturnErrorMessage() throws Exception {
+        //given
+        MemberRequest newMember = new MemberRequest("Tito", "facebook.com", "instagram.com", "linkedin.com", "image.jpg", "NEW description", null);
+        //when
+
+        //then
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put("/members/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newMember)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors").value("organizationId: must not be null"));
     }
 }
