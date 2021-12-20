a3e2179211acc81749ac76066094161d0b964eb7
Yane05 <yane.05@hotmail.com>
2021-12-19 22:16:11 -0300

    ContactController Test

--- src/main/java/com/alkemy/ong/services/ContactService.java
+++ src/main/java/com/alkemy/ong/services/ContactService.java
@@ -1,11 +1,14 @@
 package com.alkemy.ong.services;
 
+import com.alkemy.ong.dtos.responses.ContactListDTO;
 import com.alkemy.ong.entities.Contact;
 import com.alkemy.ong.repositories.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -14,8 +17,13 @@ public class ContactService {
     @Autowired
     private ContactRepository contactRepository;
 
-    public List<Contact> findAllContacts() {
-        return contactRepository.findAll();
+    public List<ContactListDTO> findAllContacts() {
+        List<Contact> contactList = contactRepository.findAll();
+        List<ContactListDTO> contactDTOList = new ArrayList<>();
+        for (Contact c : contactList) {
+            contactDTOList.add(new ContactListDTO(c));
+        }
+        return contactDTOList;
     }
 
     public Contact findContactById(Long id) {
