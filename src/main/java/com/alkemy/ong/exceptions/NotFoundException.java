c523ff562874728e49f8e49e4623f740804dd2bd
Ignacio Lòpez <agsdf12341@gmail.com>
2021-12-29 19:17:43 -0300

    Mocked Beans in TestimonialsControllerTests

--- src/main/java/com/alkemy/ong/exceptions/NotFoundException.java
+++ src/main/java/com/alkemy/ong/exceptions/NotFoundException.java
@@ -4,4 +4,9 @@ public class NotFoundException extends RuntimeException{
     public NotFoundException(String message) {
         super(message);
     }
+
+    public NotFoundException() {
+        super();
+    }
+    
 }
--- src/test/java/com/alkemy/ong/controllers/TestimonialsControllerTests.java
+++ src/test/java/com/alkemy/ong/controllers/TestimonialsControllerTests.java
@@ -1,6 +1,10 @@
 package com.alkemy.ong.controllers;
 
 import com.alkemy.ong.dtos.requests.TestimonialDTO;
+import com.alkemy.ong.entities.TestimonialEntity;
+import com.alkemy.ong.exceptions.BadRequestException;
+import com.alkemy.ong.exceptions.NotFoundException;
+import com.alkemy.ong.services.TestimonialService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,6 +18,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import javax.transaction.Transactional;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doThrow;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -22,18 +29,24 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @ExtendWith(MockitoExtension.class)
 public class TestimonialsControllerTests {
+    
+    @MockBean
+    private TestimonialService testService;
+    
     @Autowired
     private MockMvc mockMvc;
-
-
-
+    
     @Test
     @Transactional
     @WithUserDetails(value = "admin@admin.com")
     void createSucess() throws Exception {
+        TestimonialDTO testBean = new TestimonialDTO("newName","https://www.newImage.com/image.png","newContent");
+        
+        when(testService.create(testBean)).thenReturn(new TestimonialEntity(1L, "newName", "https://www.newImage.com/image.png", "newContent", null, null, null));
+        
         mockMvc.perform( MockMvcRequestBuilders
                 .post("/testimonials")
-                .content(asJsonString(new TestimonialDTO("newName","https://www.newImage.com/image.png","newContent")))
+                .content(asJsonString(testBean))
                 .characterEncoding("utf-8")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
@@ -59,20 +72,32 @@ public class TestimonialsControllerTests {
     @Transactional
     @WithUserDetails(value = "admin@admin.com")
     void updateSucess() throws Exception {
+        TestimonialDTO testBean = new TestimonialDTO("editName","https://www.editImage.com/image.png","editContent");
+        
+        when(testService.update(1L, testBean)).thenReturn(new TestimonialEntity(1L, "editName", "https://www.editImage.com/image.png", "editContent", null, null, null));
+        
         mockMvc.perform( MockMvcRequestBuilders
                 .put("/testimonials/{id}", 1)
-                .content(asJsonString(new TestimonialDTO("editName","https://www.editImage.com/image.png","editContent")))
+                .content(asJsonString(testBean))
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("editName"))
+                .andExpect(jsonPath("$.image").value("https://www.editImage.com/image.png"))
+                .andExpect(jsonPath("$.content").value("editContent"));
     }
+    
     @Test
     @Transactional
     @WithUserDetails(value = "admin@admin.com")
     void tryUpdateWithIdNotExists() throws Exception {
+        TestimonialDTO testBean = new TestimonialDTO("editName","https://www.editImage.com/image.png","editContent");
+        
+        when(testService.update(0L, testBean)).thenThrow(NotFoundException.class);
+        
         mockMvc.perform( MockMvcRequestBuilders
-                .put("/testimonials/{id}", 123*5+10)
-                .content(asJsonString(new TestimonialDTO("editName","https://www.editImage.com/image.png","editContent")))
+                .put("/testimonials/{id}", 0)
+                .content(asJsonString(testBean))
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound());
@@ -81,9 +106,13 @@ public class TestimonialsControllerTests {
     @Transactional
     @WithUserDetails(value = "admin@admin.com")
     void tryUpdateWithValuesNull() throws Exception {
+        TestimonialDTO testBean = new TestimonialDTO("editName","https://www.editImage.com/image.png","editContent");
+        
+        when(testService.update(1L, testBean)).thenThrow(BadRequestException.class);
+        
         mockMvc.perform( MockMvcRequestBuilders
                 .put("/testimonials/{id}", 1)
-                .content(asJsonString(new TestimonialDTO()))
+                .content(asJsonString(testBean))
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
@@ -105,8 +134,9 @@ public class TestimonialsControllerTests {
     @Transactional
     @WithUserDetails(value = "admin@admin.com")
     void tryDeleteWithIdNotExists() throws Exception {
+        doThrow(new NotFoundException()).when(testService).delete(2L);
         mockMvc.perform( MockMvcRequestBuilders
-                .delete("/testimonials/{id}", 123*5+10)
+                .delete("/testimonials/{id}", 2)
                 .content(asJsonString(new TestimonialDTO("Name","https://www.Image.com/image.png","Content")))
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
